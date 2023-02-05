714d26f870ce89d2e3e24f15c79f9aac980b5a7f
Kenton Bocock <kentonbo@gmail.com>
2023-02-05 13:20:47 -0500

    probe_ margin

--- Configuration.h
+++ Configuration.h
@@ -1544,7 +1544,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -45, -10, -1.4 }
+#define NOZZLE_TO_PROBE_OFFSET { -46, -11, -1.4 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
