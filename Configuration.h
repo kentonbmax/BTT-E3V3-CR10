37a9995f2d696d56ea4ba13c457246f5f9518038
Kenton Bocock <kentonbo@gmail.com>
2023-02-08 19:52:06 -0500

    roll back some changes

--- Configuration.h
+++ Configuration.h
@@ -1544,7 +1544,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -46, -11, -1.4 }
+#define NOZZLE_TO_PROBE_OFFSET { -46, -9, -1.1 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -2020,7 +2020,7 @@
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 5
+  #define GRID_MAX_POINTS_X 3
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
