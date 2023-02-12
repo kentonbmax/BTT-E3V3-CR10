2ad260b045abe746dc5ccf850ec4c3f1304d0102
Kenton Bocock <kentonbo@gmail.com>
2023-02-12 10:26:56 -0500

    set z max height 10mm under printer max

--- Configuration.h
+++ Configuration.h
@@ -1756,7 +1756,7 @@
 
 // The size of the printable area
 #define X_BED_SIZE 300
-#define Y_BED_SIZE 300
+#define Y_BED_SIZE 200
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -1764,7 +1764,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE + 20
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 400
+#define Z_MAX_POS 390
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
 //#define J_MIN_POS 0
